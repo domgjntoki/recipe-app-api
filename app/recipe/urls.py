f28d53377c6e6f51f5d465e735e18de254df8ffe
Fredson Menezes Sumi Barreto <fredson.sumi@ufba.br>
2024-04-21 03:03:47 -0300

    filter function

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -16,6 +16,7 @@ router.register('recipes', views.RecipeViewSet)
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
 
+
 app_name = 'recipe'
 
 urlpatterns = [
